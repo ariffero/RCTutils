a45e17096c348c91a5d2e113578a95696ab4acd2
jian <jian.liu@cern.ch>
2024-05-07 11:03:54 +0200

    supporting 2022 periods

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# How to produce a run list (for Pb-Pb and pp periods in 2023)
+# How to produce a run list (for Pb-Pb and pp periods in 2023 and 2022)
 - The configuration file: config_pp.json or config_pbpb.json.
   - sheet_name: the name of the google spreadsheets, e.g., `QC_summary_data_2023_pbpb` for 2023 Pb-Pb.
   - tab_name: the name of the tab that you want to take.
@@ -7,5 +7,7 @@
   - pass_shift: select which apass to be checked. For example, "1" refers to the first apass for a detector.
   - pass_name: set the apass name which will be in the dumped file.
   - runlists -> name: indicating which run list it is, e.g., CBT, CBT_hadronPID
+  - separate_22o_test: set whether to separate LHC22o_test. Default is "False"
 - Make sure you have the certificate runlist-5dfcf12a816d.json under the same folder where you run the script. Contact Jian Liu (jian.liu@cern.ch) for the certificate.
 - `python3 runlist.py config_pp.json`
+- Take `config_pp_2022.json` as the reference configuration file for 2022 periods 
--- runlist.py
+++ runlist.py
@@ -16,6 +16,35 @@ def read_config(file_path):
 def get_detector_column_index(detector, header_row, pass_id):
     return header_row.index(detector) + pass_id + 1
 
+def check_run_quality_2022(run_row, detector_indices, qualities, current_period, period_column_index, allowed_periods, separate_22o_test):
+    period_raw = run_row[period_column_index].strip()
+    period = ''
+    if period_raw == '' and (current_period == "LHC22o_test" or current_period == "LHC22o") and separate_22o_test == "True":
+        current_period = "LHC22o"
+        period = current_period
+    if period_raw.startswith('LHC22'):
+        period = period_raw
+        current_period = period
+    elif period_raw.startswith('22o_test'):
+        period = "LHC22o_test"
+        current_period = period
+    else:
+        period = current_period
+    #period = run_row[period_column_index].strip() or current_period
+    #if allowed_periods and period not in allowed_periods:
+    if "LHC22o" in allowed_periods and separate_22o_test == "False":  
+        if allowed_periods and current_period not in allowed_periods and current_period != "LHC22o_test" or period_raw == 'BAD':
+            return False, period
+    else: 
+        if allowed_periods and current_period not in allowed_periods or period_raw == 'BAD':
+            return False, period
+    for detector, quality_flags in qualities.items():
+        if run_row[detector_indices[detector]-1].strip() not in quality_flags:
+            #print(detector,run_row[detector_indices[detector]-1].strip())
+            return False, period
+        #print(detector,run_row[detector_indices[detector]-1].strip())
+    return True, period
+
 def check_run_quality(run_row, detector_indices, qualities, current_period, period_column_index, allowed_periods):
     period = run_row[period_column_index].strip() or current_period
     if allowed_periods and period not in allowed_periods:
@@ -36,13 +65,17 @@ client = gspread.authorize(credentials)
 current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 for sheet_config in config['sheets']:
     spreadsheet = client.open(sheet_config.get('sheet_name'))
+    #print(f'sheet name: {spreadsheet}')
     allowed_periods = sheet_config.get('periods', None)
     worksheet = spreadsheet.worksheet(sheet_config['tab_name'])
     pass_id = int(sheet_config.get('pass_shift', 1))
     pass_name = sheet_config.get('pass_name', None)
+    separate_22o_test  =  sheet_config.get('separate_22o_test', False)
+ 
     for runlist_config in sheet_config['runlists']:
         header_row_period = worksheet.row_values(1)
         header_row = worksheet.row_values(2)
+        #print(header_row)
         unique_periods = set()
         default_periods = set()
 
@@ -53,10 +86,14 @@ for sheet_config in config['sheets']:
         rows = worksheet.get_all_values()
 
         for row in rows[3:]:
-            is_good_run, current_period = check_run_quality(row, detector_indices, runlist_config['detectors'], current_period, period_column_index, allowed_periods)
+            if sheet_config.get('sheet_name') == "QC_summary_data_2022":
+                is_good_run, current_period = check_run_quality_2022(row, detector_indices, runlist_config['detectors'], current_period, period_column_index, allowed_periods, separate_22o_test)
+            else: is_good_run, current_period = check_run_quality(row, detector_indices, runlist_config['detectors'], current_period, period_column_index, allowed_periods)
             default_periods.add(current_period)
             if is_good_run:
-                runlist.append(row[3])
+                if sheet_config.get('sheet_name') == "QC_summary_data_2022":
+                    runlist.append(row[0])
+                else: runlist.append(row[3])
 
         if allowed_periods:
             unique_periods = allowed_periods
